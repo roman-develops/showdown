927bd0d62b592c5c961282faf7132a14c976916f
KryukovRoman <kriukov-ra@ic.ntcees.ru>
2024-06-30 01:05:13 +0300

    Add auth exception handling

--- src/main/java/dev/showdown/config/WebSocketConfig.java
+++ src/main/java/dev/showdown/config/WebSocketConfig.java
@@ -3,21 +3,31 @@ package dev.showdown.config;
 import dev.showdown.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompConversionException;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
 import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationToken;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.handler.ExceptionWebSocketHandlerDecorator;
+import org.springframework.web.socket.handler.WebSocketHandlerDecoratorFactory;
+
+import java.util.Optional;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -33,6 +43,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
+        registry.setErrorHandler(new StompSubProtocolErrorHandler());
     }
 
     @Override
@@ -62,18 +73,24 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 }
 
                 String authorizationHeader = accessor.getFirstNativeHeader(HttpHeaders.AUTHORIZATION);
-                tokenService.extractTokenFromBearerString(authorizationHeader).ifPresent(token -> {
-                    try {
-                        accessor.setUser(authenticationManager.authenticate(new BearerTokenAuthenticationToken(token)));
-                    } catch (InvalidBearerTokenException e) {
-                        System.out.println(e); // TODO Add logger. Return error message
-                    } catch (Exception e) {
-                        System.out.println(e); // TODO Add logger. Return error message
-                    }
-                });
+                Optional<String> tokenOptional = tokenService.extractTokenFromBearerString(authorizationHeader);
+
+                if (authorizationHeader == null || tokenOptional.isEmpty()) {
+                    throw new InvalidBearerTokenException("Unable to extract token");
+                }
+
+                accessor.setUser(authenticationManager.authenticate(new BearerTokenAuthenticationToken(tokenOptional.get())));
 
                 return message;
             }
         });
+    }
+
+    private Message<?> createErrorMessage(StompHeaderAccessor accessor, String errorMessage) {
+        StompHeaderAccessor errorAccessor = StompHeaderAccessor.create(StompCommand.ERROR);
+        errorAccessor.setMessage(errorMessage);
+        errorAccessor.setSessionId(accessor.getSessionId());
+
+        return MessageBuilder.createMessage(errorMessage.getBytes(), errorAccessor.getMessageHeaders());
     }
 }
--- src/main/java/dev/showdown/controller/CommonControllerAdvice.java
+++ src/main/java/dev/showdown/controller/CommonControllerAdvice.java
@@ -1,13 +1,30 @@
 package dev.showdown.controller;
 
+import dev.showdown.dto.TokenDto;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.security.Principal;
+
 @ControllerAdvice
+@RequiredArgsConstructor
 public class CommonControllerAdvice {
+
+    @MessageExceptionHandler
+    @SendToUser("/queue/errors")
+    public String handleException(AuthorizationDeniedException ex, Principal principal) {
+        return "Access denied!";
+    }
 
     @ExceptionHandler
     public ResponseEntity<String> responseStatusException(ResponseStatusException ex) {
